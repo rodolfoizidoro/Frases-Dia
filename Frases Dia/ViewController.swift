cbec3d5c01f1cc0209748e6d2eabbccefb51b3ac
rodolfoizidoro <rodolfoizidoro@gmail.com>
2017-03-02 13:57:58 -0300

    Frases do dia
    Curso Ios

--- Frases Dia/ViewController.swift
+++ Frases Dia/ViewController.swift
@@ -14,6 +14,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBOutlet weak var legendaFrase: UILabel!
+    @IBAction func gerarFrase(_ sender: Any) {
+        
+        var frases:[String] = [];
+        
+        frases.append("Toda ação humana, quer se torne positiva ou negativa, precisa depender de motivação.");
+        
+        frases.append("A verdadeira motivação vem de realização, desenvolvimento pessoal, satisfação no trabalho e reconhecimento.");
+        
+        frases.append("Só se pode alcançar um grande êxito quando nos mantemos fiéis a nós mesmos.");
+        
+        frases.append("Quando uma criatura humana desperta para um grande sonho e sobre ele lança toda a força de sua alma, todo o universo conspira a seu favor.");
+        
+        let indice = Int(arc4random_uniform(4));
+        
+        legendaFrase.text = frases[indice];
+        
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
